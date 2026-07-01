edb14c3884edcd1f4d06f47c4285998b1f6d8db9
MohammedDev-yt <animeworldday2005@gmail.com>
2026-07-01 08:18:50 +0530

    start.sh

---
+++ start.sh
@@ -0,0 +1,22 @@
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
+
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /EvaMaria
+fi
+cd /EvaMaria
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
+
+# ------------------------- #
+# Don't Remove Credit 
+# Owner @Mr_Mohammed_29
+# ------------------------- #
